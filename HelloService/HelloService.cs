df539a047c407f08722ac1e0113aed0b08e21518
Guilherme Felix da Silva <42627524+GuilhermeFelix@users.noreply.github.com>
2019-04-11 01:53:24 -0300

    Class 2
    Introduction to webservices, teaching to create a simple service to join the word "Hello" with the name entered by the user.
    In this lesson I showed how to create a wcf in another way and also illustrated how to include an application console to easily activate the service

---
+++ HelloService/HelloService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "HelloService" no arquivo de código e configuração ao mesmo tempo.
+    public class HelloService : IHelloService
+    {
+        public string GetMessage(string name)
+        {
+            return ("Hello ") + name;
+        }
+    }
+}
